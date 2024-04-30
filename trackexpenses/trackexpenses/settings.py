69209a32dd7e085fd9b6ce7f4c200cec75a5f156
DikshyaPaudel <dikshyapaudel9@gmail.com>
2024-04-30 13:15:58 +0545

    Made some changes

--- trackexpenses/trackexpenses/settings.py
+++ trackexpenses/trackexpenses/settings.py
@@ -142,7 +142,7 @@ MESSAGE_TAGS={
 }
 
 
-EMAIL_HOST_PASSWORD = 'vshf fzsj arvr fdnb'
+EMAIL_HOST_PASSWORD = '#####'
 EMAIL_HOST_USER='dikshyapaudel9@gmail.com'
 DEFAULT_FROM_EMAIL='dikshyapaudel9@gmail.com'
 EMAIL_HOST='smtp.gmail.com'
